3dc213185413ff699b82ad3c815d7847dcd32383
Pubby <pubby8@gmail.com>
2023-03-13 00:51:27 -0500

    Add example build helper

---
+++ examples/build_all.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+../nesfab cnrom/cnrom.cfg
+../nesfab hang_glider/hang_glider.cfg
+../nesfab hello_world/hello_world.cfg
+../nesfab maze/maze.cfg
+../nesfab mmc1/mmc1.cfg
+../nesfab scanline_irq/scanline_irq.cfg
+../nesfab text/text.cfg

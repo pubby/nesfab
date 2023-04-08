4e04cccadc0d4554886e86b4a515c54fb6bb9852
Pubby <pubby8@gmail.com>
2023-04-07 22:05:22 -0500

    Update tables

--- src/ext_lex_tables.hpp
+++ src/ext_lex_tables.hpp
@@ -42,7 +42,7 @@ inline std::string_view token_string(token_type_t type)
 #define ext_lex_TOK_KEY_CASES \
 
 constexpr token_type_t TOK_LAST_STATE = 6;
-constexpr token_type_t TOK_START = 7;
+constexpr token_type_t TOK_START = 24;
 extern unsigned const lexer_ec_table[256];
 extern token_type_t const lexer_transition_table[972];
 } // namespace ext_lex

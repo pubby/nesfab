24dbcbd0135963692cd6e8ed0429dd5dee54bfba
Pubby <pubby8@gmail.com>
2023-08-24 15:25:19 -0500

    Add missing files

---
+++ src/o_locator.cpp
@@ -0,0 +1,52 @@
+#include "o_locator.hpp"
+
+#include "globals.hpp"
+#include "ir.hpp"
+
+bool o_optimize_locators(log_t* log, ir_t& ir)
+{
+    bool changed = false;
+
+    for(cfg_node_t const& cfg : ir)
+    for(ssa_ht ssa_it = cfg.ssa_begin(); ssa_it; ++ssa_it)
+    {
+        unsigned input_size = ssa_it->input_size();
+
+        for(unsigned i = 0; i < input_size; ++i)
+        {
+            ssa_value_t const input = ssa_it->input(i);
+
+            if(!input.is_locator())
+                continue;
+
+            locator_t loc = input.locator();
+            if(loc.is() == IS_BANK)
+                loc.set_offset(0); // Banks ignore offsets.
+
+            switch(loc.lclass())
+            {
+            default: 
+                break;
+
+            case LOC_NAMED_LABEL:
+                // Named label banks should map to the same bank locator:
+                if(loc.is() == IS_BANK)
+                {
+                    global_ht const g = loc.global();
+                    if(g->gclass() == GLOBAL_CONST)
+                        loc = locator_t::gconst(g->handle<const_ht>()).with_is(IS_BANK);
+                }
+                break;
+            }
+
+            if(loc != input.locator())
+            {
+                ssa_it->link_change_input(i, loc);
+                changed = true;
+            }
+        }
+    }
+
+    return changed;
+}
+
---
+++ src/o_locator.hpp
@@ -0,0 +1,11 @@
+#ifndef O_LOCATOR_HPP
+#define O_LOCATOR_HPP
+
+#include "debug_print.hpp"
+#include "ir_decl.hpp"
+
+class fn_t;
+
+bool o_optimize_locators(log_t* log, ir_t& ir);
+
+#endif
